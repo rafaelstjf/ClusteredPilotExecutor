250d80f1fd9c67b892339d05be547dd20d2c52c9
Rafael Terra <rafaelstjf@gmail.com>
2025-10-02 10:29:15 -0300

    added the setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="adaptive-parsl-executor",
+    version="0.1.0",
+    description="AdaptivePilotExecutor for Parsl: supports pilot jobs, clustering, and dynamic task submission.",
+    author="Rafael Terra",
+    author_email="rafaelstjf@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "parsl>=1.2",   # adjust to your Parsl version
+        "pyzmq",
+        "networkx",
+        "sqlite3",
+        "pandas",
+        "matplotlib"
+    ],
+    entry_points={
+        'console_scripts': [
+            'adaptive_worker=adaptive_executor.worker:main',
+        ],
+    },
+    python_requires='>=3.8',
+    include_package_data=True,
+)
